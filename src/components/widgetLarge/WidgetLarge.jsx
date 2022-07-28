2cd050b45d53d78210f0d97ec2589238d2b7f2c0
Ben Limo <bensokiplimo@gmail.com>
2022-07-28 12:55:54 +0300

    add large home widget - transactions table

--- src/components/widgetLarge/WidgetLarge.jsx
+++ src/components/widgetLarge/WidgetLarge.jsx
@@ -1,7 +1,48 @@
 import './widgetlarge.css'
 
 export default function WidgetLarge() {
+  const Button = ({type}) => {
+    return <button className={"widgetLargeButton " + type}>{type}</button>
+  }
   return (
-    <div className='widgetlarge'>WidgetLarge</div>
+    <div className='widgetlarge'>
+      <h3 className="widgetLargeTitle">Latest Transactions</h3>
+      <table className="widgetLargeTable">
+        <tr className="widgetLargeTr">
+          <th className="widgetLargeTh">Customer</th>
+          <th className="widgetLargeTh">Product</th>
+          <th className="widgetLargeTh">Amount</th>
+          <th className="widgetLargeTh">Date</th>
+          <th className="widgetLargeTh">Status</th>
+        </tr>
+        <tr className="widgetLargeTr">
+          <td className="widgetLargeCustomer">Safaricom</td>
+          <td className="widgetLargeCategory">Laptops</td>
+          <td className="widgetLargeAmount">Ksh.1,204,534</td>
+          <td className="widgetLargeDate">27/07/2022</td>
+          <td className="widgetLargeStatus">
+            <Button type="Approved" />
+          </td>
+        </tr>
+        <tr className="widgetLargeTr">
+          <td className="widgetLargeCustomer">Safaricom</td>
+          <td className="widgetLargeCategory">Laptops</td>
+          <td className="widgetLargeAmount">Ksh.1,204,534</td>
+          <td className="widgetLargeDate">27/07/2022</td>
+          <td className="widgetLargeStatus">
+            <Button type="Declined" />
+          </td>
+        </tr>
+        <tr className="widgetLargeTr">
+          <td className="widgetLargeCustomer">Safaricom</td>
+          <td className="widgetLargeCategory">Laptops</td>
+          <td className="widgetLargeAmount">Ksh.1,204,534</td>
+          <td className="widgetLargeDate">27/07/2022</td>
+          <td className="widgetLargeStatus">
+            <Button type="Pending" />
+          </td>
+        </tr>
+      </table>
+    </div>
   )
 }
